e6070362bfa45bd007ee104fcaca8b01642a86a3
choniyuazwan <choniyuazwan@gmail.com>
2020-02-22 09:18:15 +0700

    feat(province): add validation form

--- src/Container/Province/Add.js
+++ src/Container/Province/Add.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import { Spinner, Form, Button, Row, Col, Card } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import { options, url } from "../../Util/Api";
 
 function ProvinceAdd(props) {
   const [listCountry, setListCountry] = useState([]);
-  const [data, setData] = useState({ country_id: '', name: '' });
   const [showLoading, setShowLoading] = useState(false);
 
   const countryUrl = `${url.country}?size=9999`;
@@ -22,9 +23,8 @@ function ProvinceAdd(props) {
     fetchCountryData();
   }, []);
 
-  const save = (e) => {
+  const save = (data) => {
     setShowLoading(true);
-    e.preventDefault();
     const payload = {country_id: data.country_id, name: data.name };
     axios.post(provinceUrl, payload, options)
       .then((result) => {
@@ -33,10 +33,10 @@ function ProvinceAdd(props) {
       }).catch((error) => setShowLoading(false));
   };
 
-  const onChange = (e) => {
-    e.persist();
-    setData({...data, [e.target.name]: e.target.value});
-  };
+  const schema = yup.object({
+    country_id: yup.string().required(),
+    name: yup.string().required()
+  });
 
   return (
     <div>
@@ -50,37 +50,68 @@ function ProvinceAdd(props) {
         <Row>
           <Col><h5>Province Add</h5></Col>
         </Row>
-        <Form onSubmit={save}>
-          <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
-            <Form.Label column sm={3} className="text-right">
-              Country
-            </Form.Label>
-            <Col sm={4}>
-              <Form.Control as="select" size="sm" name="country_id" value={data.country_id} onChange={onChange} >
-                <option>Choose...</option>
-                {
-                  listCountry.map((item, index) => (
-                    <option key={index} value={item.id}>{item.name}</option>
-                  ))
-                }
-              </Form.Control>
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} controlId="formHorizontalName">
-            <Form.Label column sm={3} className="text-right">
-              Name
-            </Form.Label>
-            <Col sm={4}>
-              <Form.Control size="sm" type="text" name="name" id="name" placeholder="Name" value={data.name} onChange={onChange} />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row}>
-            <Col sm={{ offset: 3 }}>
-              <Button size="sm" type="submit">Submit</Button> &nbsp;
-              <Button size="sm" type="button" variant="success" href="/province">Back</Button>
-            </Col>
-          </Form.Group>
-        </Form>
+
+        <Formik
+          validationSchema={schema}
+          onSubmit={save}
+          initialValues={{
+            country_id: '',
+            name: '',
+          }}
+        >
+          {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              isValid,
+              errors,
+            }) => {
+            const valid = !!isValid && values.country_id !== '';
+            return (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
+                  <Form.Label column sm={3} className="text-right">
+                    Country
+                  </Form.Label>
+                  <Col sm={4}>
+                    <Form.Control as="select" size="sm" name="country_id" value={values.country_id} onChange={handleChange} isValid={touched.country_id && !errors.country_id} isInvalid={!!errors.country_id} >
+                      <option value="">Choose...</option>
+                      {
+                        listCountry.map((item, index) => (
+                          <option key={index} value={item.id}>{item.name}</option>
+                        ))
+                      }
+                    </Form.Control>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.country_id}
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalName">
+                  <Form.Label column sm={3} className="text-right">
+                    Name
+                  </Form.Label>
+                  <Col sm={4}>
+                    <Form.Control size="sm" type="text" name="name" id="name" placeholder="Name" value={values.name} onChange={handleChange} isValid={touched.name && !errors.name} isInvalid={!!errors.name} />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.name}
+                    </Form.Control.Feedback>
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                  <Col sm={{ offset: 3 }}>
+                    <Button size="sm" type="submit" disabled={!valid} >Submit</Button> &nbsp;
+                    <Button size="sm" type="button" variant="success" href="/province">Back</Button>
+                  </Col>
+                </Form.Group>
+              </Form>
+            )
+          }}
+        </Formik>
       </Card>
     </div>
   );
